d4d72261198460019e2882b33857620117db40d3
Giulia <g.bonicolini@key2.it>
2024-10-14 12:27:18 +0200

    creazione card e social icon

---
+++ src/app/library/card/card.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { BottoneComponent } from "../bottone/bottone.component";
+
+@Component({
+  selector: 'app-card',
+  standalone: true,
+  imports: [BottoneComponent],
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.scss'
+})
+export class CardComponent {
+  testoBottCard = 'Book now';
+
+}
--- src/app/library/home/home.component.ts
+++ src/app/library/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component, inject } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { SelezionaImmagineService } from '../../core/services/seleziona-immagine.service';
+import { BottoneComponent } from "../bottone/bottone.component";
+import { CardComponent } from '../card/card.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, BottoneComponent, CardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
